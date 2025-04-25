314490df56033dd72991df84d72874be302cc879
charlescleyton <cleyton.master@gmail.com>
2025-04-25 16:35:17 -0300

    feat: show file data convenios.json

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -11,8 +11,16 @@ class SimulationController extends Controller
         $instituicaoJson = json_decode(Storage::get('instituicoes.json', true));
         return $this->getData($instituicaoJson);
     }
+
+    public function getAgreements()
+    {
+        $conveniosJson = json_decode(Storage::get('convenios.json', true));
+        return $this->getData($conveniosJson);
+    }
+
     private function getData($request){
 
-        return collect($request)->select('chave', 'valor')->toArray();
+        return collect($request)->select('chave', 'valor');
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\SimulationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/instituicoes', [SimulationController::class, 'getInstitutions']);
+Route::get('/convenios', [SimulationController::class, 'getAgreements']);
